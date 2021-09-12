88de960a4c4dd31df254c7701da3c8ae226bb58f
Hussam Albadri <hussamalbadri2@gmail.com>
2021-09-13 01:41:31 +0300

    lab1

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class footer extends React.Component{
+    render(){
+        return(
+            <h3>Author: Hussam Albadri</h3>
+        )
+    }
+}
+export default footer;
